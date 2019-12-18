53a93bcab19426e737131a1814eaa78f568ac5b6
mohsen shabanian - work <mohsen1299@gmail.com>
2019-12-18 06:06:40 -0800

    add avatar

--- src/components/layout.js
+++ src/components/layout.js
@@ -32,18 +32,23 @@ import Divider from '@material-ui/core/Divider';
 import Drawer from '@material-ui/core/Drawer';
 import Hidden from '@material-ui/core/Hidden';
 import IconButton from '@material-ui/core/IconButton';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import MailIcon from '@material-ui/icons/Mail';
 import MenuIcon from '@material-ui/icons/Menu';
+import HomeIcon from '@material-ui/icons/Home';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import ContactMailIcon from '@material-ui/icons/ContactMail';
+import ArtTrackIcon from '@material-ui/icons/ArtTrack';
+import BurstModeIcon from '@material-ui/icons/BurstMode';
+import LibraryBooksIcon from '@material-ui/icons/LibraryBooks';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Avatar from '@material-ui/core/Avatar';
 
-const drawerWidth = 240;
+const drawerWidth = 200;
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -57,8 +62,9 @@ const useStyles = makeStyles(theme => ({
   },
   appBar: {
     [theme.breakpoints.up('sm')]: {
-      width: `calc(100% - ${drawerWidth}px)`,
-      marginLeft: drawerWidth,
+      //width: `calc(100% - ${drawerWidth}px)`,
+      //marginLeft: drawerWidth,
+      zIndex: theme.zIndex.drawer + 1,
     },
   },
   menuButton: {
@@ -75,6 +81,9 @@ const useStyles = makeStyles(theme => ({
     flexGrow: 1,
     padding: theme.spacing(3),
   },
+  avatar:{
+    marginRight: theme.spacing(1),
+  }
 }));
 
 const Layout = (props) => {
@@ -93,22 +102,33 @@ const Layout = (props) => {
       <div className={classes.toolbar} />
       <Divider />
       <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
+        <ListItem button>
+          <ListItemIcon> <HomeIcon fontSize="large"/> </ListItemIcon>
+          <ListItemText primary={"Home"} />
+        </ListItem>
+        <ListItem button>
+          <ListItemIcon> <AccountCircleIcon fontSize="large"/> </ListItemIcon>
+          <ListItemText primary={"About"} />
+        </ListItem>
+        <ListItem button>
+          <ListItemIcon> <ArtTrackIcon fontSize="large"/> </ListItemIcon>
+          <ListItemText primary={"resume"} />
+        </ListItem>
+        <ListItem button>
+          <ListItemIcon> <BurstModeIcon fontSize="large"/> </ListItemIcon>
+          <ListItemText primary={"portofolio"} />
+        </ListItem>
+        <ListItem button>
+          <ListItemIcon> <LibraryBooksIcon fontSize="large"/> </ListItemIcon>
+          <ListItemText primary={"blogs"} />
+        </ListItem>
+        <ListItem button>
+          <ListItemIcon> <ContactMailIcon fontSize="large"/> </ListItemIcon>
+          <ListItemText primary={"contact"} />
+        </ListItem>
       </List>
       <Divider />
-      <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-      </List>
+      
     </div>
   );
 
@@ -126,6 +146,7 @@ const Layout = (props) => {
           >
             <MenuIcon />
           </IconButton>
+          <Avatar className={classes.avatar} alt="Mohsen Shabanian" src="/images/me.png" />
           <Typography variant="h6" noWrap>
             Mohsen Shabanian
           </Typography>
